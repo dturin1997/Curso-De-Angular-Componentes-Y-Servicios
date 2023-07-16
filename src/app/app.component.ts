16365049a30a546874ed31bc06615c64379e853c
Dario Jose Turin Scharff <dturin1997@gmail.com>
2023-07-16 00:07:22 -0500

    Video-05-platzi: Componente para producto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,34 @@ export class AppComponent {
   // imgParent = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGU57yGujJ6MSt9xUpfBcLDaM1_Zh7vSTujFBiviDquzFvMdi8Zx4vx-0KPonopKG2368&usqp=CAU';
   imgParent = '';
 
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+    },
+    {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg',
+    },
+    {
+      id: '3',
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg',
+    },
+    {
+      id: '4',
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg',
+    },
+  ];
+
   onLoaded(img: string) {
-    console.log('log padre => ', img );
+    console.log('log padre => ', img);
   }
 }
---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
+})
+export class ProductComponent implements OnInit {
+  @Input('myProduct') product: Product = {
+    id: '',
+    price: 0,
+    image: '',
+    name: '',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
